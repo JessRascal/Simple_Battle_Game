a54121c603161956e72696217538ebe70f4536d6
Jess Rascal <Jess@Jessey.uk>
2016-01-28 20:14:34 +0000

    Initial Commit
    All files added.

---
+++ Lecture52 - OOP Exercise/Character.swift
@@ -0,0 +1,92 @@
+//
+//  Character.swift
+//  Lecture52 - OOP Exercise
+//
+//  Created by Michael Jessey on 19/01/2016.
+//  Copyright © 2016 JustOneJess. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class Character {
+    
+    private var _hp: Int = 100
+    private var _attackPower: Int = 20
+    private var _name: String = "CharacterName"
+    private var _characterChoice: Int = 1
+    
+    // Getters (computed properties).
+    var hp: Int {
+        get { return _hp }
+    }
+    
+    var attackPower: Int {
+        get { return _attackPower }
+    }
+    
+    var name: String {
+        get { return _name }
+    }
+    
+    var characterChoice: Int {
+        get {return _characterChoice }
+    }
+    
+    // Initialiser.
+    init(startHp: Int, startAttackPower: Int, startName: String, characterChoiceIn: Int) {
+        self._hp = startHp
+        self._attackPower = startAttackPower
+        self._name = startName
+        self._characterChoice = characterChoiceIn
+    }
+    
+    // Perform an attack.
+    func performAttack(attackingPlayer: Character, playerBeingAttacked: Character) {
+        // Randomise the attack power before attacking.
+        attackingPlayer.randomiseAttackPower()
+        
+        if playerBeingAttacked._hp > 0 {
+            playerBeingAttacked._hp -= attackingPlayer._attackPower
+        }
+    }
+    
+    // Check if still alive (i.e. HP above 0).
+    func isAlive () -> Bool {
+        if self._hp <= 0 {            
+            return false
+        }
+        return true
+    }
+    
+    // Randomise the attack power.
+    func randomiseAttackPower() {
+        self._attackPower = Int(arc4random_uniform(UInt32(_attackPower)))
+    }
+    
+    // Reset the attack power back to the default.
+    func resetAttackPower() {
+        self._attackPower = 20
+    }
+    
+    // Set the player's image (and flip the image if needed).
+    func setPlayerImage(playerNumberIn: Int, imageChoiceIn: Int) -> UIImage {
+        if imageChoiceIn == 1 {
+            if playerNumberIn == 1 {
+                let flippedImage = UIImage(CGImage: UIImage(named: "player")!.CGImage!, scale: 1.0, orientation: .UpMirrored)
+                return flippedImage
+            } else {
+                return UIImage(named: "player")!
+            }
+            
+        } else {
+            if playerNumberIn == 2 {
+                let flippedImage = UIImage(CGImage: UIImage(named: "enemy")!.CGImage!, scale: 1.0, orientation: .UpMirrored)
+                return flippedImage
+            } else {
+                return UIImage(named: "enemy")!
+            }
+        }
+    }
+}
--- Lecture52 - OOP Exercise/ViewController.swift
+++ Lecture52 - OOP Exercise/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var player1HpLabel: UILabel!
+    @IBOutlet weak var player2HpLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var player1NameLabel: UILabel!
+    @IBOutlet weak var player2NameLabel: UILabel!
+    @IBOutlet weak var player1AttackButton: UIButton!
+    @IBOutlet weak var player2AttackButton: UIButton!
+    @IBOutlet weak var player1Image: UIImageView!
+    @IBOutlet weak var player2Image: UIImageView!
+    @IBOutlet weak var restartButton: UIButton!
+    @IBOutlet weak var player1StackView: UIStackView!
+    @IBOutlet weak var player2StackView: UIStackView!
+    @IBOutlet weak var player1NameField: UITextField!
+    @IBOutlet weak var player2NameField: UITextField!
+    @IBOutlet weak var player1SegmentedControl: UISegmentedControl!
+    @IBOutlet weak var player2SegmentedControl: UISegmentedControl!
+    
+    
+    var gameInstance: Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Initialise the game and the set up screen.
+        gameInstance = Game(vc: self)
+        gameInstance.setUpGame()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func player1Attack(sender: AnyObject) {
+        // Temporarily disable the other player's 'Attack' button. (Commented out in favour of a turn based approach.)
+        //gameInstance.tempDisableAttack(player2AttackButton)
+        
+        // Disable player 1's 'Attack' button and enable player 2's.
+        player1AttackButton.enabled = false
+        player2AttackButton.enabled = true
+        // Perform the attack.
+        gameInstance.performAttack(gameInstance.player1, playerBeingAttackedIn: gameInstance.player2)
     }
 
-
+    @IBAction func player2Attack(sender: AnyObject) {
+        // Temporarily disable the other player's 'Attack' button. (Commented out in favour of a turn based approach.)
+        //gameInstance.tempDisableAttack(player1AttackButton)
+        
+        // Disable player 2's 'Attack' button and enable player 1's.
+        player1AttackButton.enabled = true
+        player2AttackButton.enabled = false
+        // Perform the attack.
+        gameInstance.performAttack(gameInstance.player2, playerBeingAttackedIn: gameInstance.player1)
+    }
+    
+    @IBAction func restartButtonTapped(sender: AnyObject) {
+        // Restart the game when the 'Restart' button is pressed.
+        gameInstance.restartGame()
+    }
+    
+    @IBAction func player1AcceptButtonTapped(sender: AnyObject) {
+        // Create player 1.
+        gameInstance.createPlayer(1, hpIn: 100, attackPowerIn: 20, nameIn: player1NameField.text!, imageChoiceIn: player1SegmentedControl.selectedSegmentIndex)
+    }
+    
+    @IBAction func player2AcceptButtonTapped(sender: AnyObject) {
+        // Create player 2.
+        gameInstance.createPlayer(2, hpIn: 100, attackPowerIn: 20, nameIn: player2NameField.text!, imageChoiceIn: player2SegmentedControl.selectedSegmentIndex)
+    }
 }
 
